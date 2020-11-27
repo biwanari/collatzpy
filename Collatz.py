f9811001075d9b286c41d3c9306afcb816767589
biwanari <59851325+biwanari@users.noreply.github.com>
2020-11-27 21:56:47 +0100

    Add files via upload

--- Collatz.py
+++ Collatz.py
@@ -14,13 +14,14 @@ def generateCollatzSeries(number):
     return list
 
 def seriesAnalysis(number, collatzseries):
-    even = [ x for x in collatzseries ]
-    odd  = [ x for x in collatzseries if x%2==1 ]
+    
+    odd  = [ i for i in collatzseries if i%2==1 ]
+    even = [ i for i in collatzseries if i%2==0 ]
 
     return f"""
     number : {number},
        - collat'z series{collatzseries}, length -> {len(collatzseries)}
-       
+
        - even number in series 
              {even}, length -> {len(even)}
        
@@ -28,26 +29,10 @@ def seriesAnalysis(number, collatzseries):
              {odd}, length -> {len(even)}
        """
 
-def generateSeriesOfCollatzSuccession(number, collatzSeries):
-    
-    generatinglist = []
-    i = 1
-    while i < number:
-        generatinglist.append( generateCollatzSeries(i) )
-        i += 1
-    return generatingList    
-
-def printPlotOfCollatzSerie(number, collatzseries):
-    x = [ i for i in collatzseries if i%2==0 ]
-    y  = [ j for j in collatzseries if j%2==1 ]
-    #p = plt.bar( np.array(x), np.array(y),width=0.8, color='green','orange' )
-    p = plt.plot(np.array(x), np.array(y))
-    p.title('even and odd number in colletz series')
-    p.xlim(len(x)-1)
-    p.ylin(len(y)-1)
-    p.xlabel(even)
-    p.ylabel(odd)
-    return p.show()
+def generateMoreSeries(number):
+    for i in range(2, 21):
+        print(seriesAnalysis(i, generateCollatzSeries(i)))
+        print("  ")
 
 if __name__ == "__main__":
     pass
